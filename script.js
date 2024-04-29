342a2f5b1c857fe66888112800d4fd8580ac81f3
Axel Martinez <axel40577213@gmail.com>
2024-04-29 17:57:39 -0300

    tp nuevo bazarsur

--- script.js
+++ script.js
@@ -64,3 +64,57 @@ function vaciarCarrito() {
     }
     return false;
 }
+
+let signUp = document.getElementById("signUp");
+let signIn = document.getElementById("signIn");
+let nameInput = document.getElementById("nameInput");
+let title = document.getElementById("title");
+
+signIn.onclick = function() {
+    nameInput.style.maxHeight = "0";
+    title.innerHTML = "Login";
+    signUp.classList.add("disable");
+    signIn.classList.remove("disable");
+}
+
+signUp.onclick = function() {
+    nameInput.style.maxHeight = "60px";
+    title.innerHTML = "Registro";
+    signUp.classList.remove("disable");
+    signIn.classList.add("disable");
+}
+
+function App () {}
+
+    window.onload = function(ovent) {
+        var app = new App();
+        window.app = app;
+    }
+
+    App.prototype.processingButton = function(event) {
+
+        const btn = event.currentTarget;
+        const carrusellist = event.currentTarget.parentNode;
+        const tranck = event.currentTarget.parentNode.querySelector('#track');
+        const carrusel = track.querySelectorAll('.carrusel');
+
+        const carruselWidth = carrusel[0].offsetWidth;
+        const trackWidth = track.offsetWidth
+        const listWidth =carrusellist.offsetWidth;
+
+        tranck.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0,-2)*-1)
+        btn.dataset.button == "button-prev" ? prevAction(leftPosition,carruselWidth, track) : nextAction(leftPosition,trackWidth, listWidth, carruselWidth, track)
+    }
+
+    let prevAction = (leftPosition, carruselWidth, track) => {
+        if (leftPosition > 0){
+            track.style.left = `${-1 * (leftPosition - carruselWidth)}px`;
+        }
+    }
+
+    let nextAction = (leftPosition, trackWidth, listWidth, carruselWidth, track) => {
+        if (leftPosition < (trackWidth - listWidth)) {
+            track.style.left = `${-1 * (leftPosition + carruselWidth)}px`;
+        }
+    }
+
