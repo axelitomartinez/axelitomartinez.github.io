65d5941363d2e9c4c4758cd54a5109312e8cb22a
Lucas <lzheng@frba.utn.edu.ar>
2024-05-13 20:57:59 -0300

    pequeños ajustes generales

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const carrito = document.getElementById('carrito');
 const elementos1 = document.getElementById('lista-1');
-const lista = document.querySelector('#lista-carrito tbody');
+const listaCarrito = document.querySelector('#lista-carrito > tbody');
 const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
 
 cargarEventListeners();
@@ -83,6 +83,28 @@ signUp.onclick = function() {
     signUp.classList.remove("disable");
     signIn.classList.add("disable");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function App () {}
 
